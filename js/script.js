fee2df0f69cb6d52f2fd93b6b8bcca30d4e03c89
AlvaroCalsolari <alvarocalsolari@hotmail.com>
2017-10-16 19:08:10 -0200

    fix layout and script for desktop

--- js/script.js
+++ js/script.js
@@ -1,34 +1,29 @@
-function generateRotCipher(string, rotValue) {
+function rot13Cipher(string) {
 	string = string.split('');
-	rotValue = Number(rotValue);
 
 	string = string.map(function (element) {
-		return rot13(element, rotValue);
+		return rot13(element);
 	});
 	string = string.join('');
 	return string;
 }
 
-function rot13(char, rotateValue) {
-	if (char.charCodeAt() >= 65 && char.charCodeAt() <= 90) {
-		return String.fromCharCode((((char.charCodeAt() % 65) + rotateValue) % 26) + 65);
-	} else if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
-		return String.fromCharCode((((char.charCodeAt() % 97) + rotateValue) % 26) + 97);
+function rot13(char) {
+	let isLowerCase = char.charCodeAt() >= 97 && char.charCodeAt() <= 122;
+	let isUpperCase = char.charCodeAt() >= 65 && char.charCodeAt() <= 90;
+	
+	if (isUpperCase) {
+		return String.fromCharCode((((char.charCodeAt() % 65) + 13) % 26) + 65);
+	} else if (isLowerCase) {
+		return String.fromCharCode((((char.charCodeAt() % 97) + 13) % 26) + 97);
 	}
 	return char;
 }
 
 $(document).ready(function () {
-
 	$('#decrypted').on("input", function () {
-		// Code goes here
 		let text = $('#decrypted').val();
-		let rotValue = $('#rot-value').val();
-		let cipher = generateRotCipher(text, rotValue);
-		$('#encrypted p').text(cipher);
+		let cipher = rot13Cipher(text);
+		$('#encrypted').text(cipher);
 	});
-
-	$('#switch').on("click", function () {
-		// code goes here
-	})
 });
